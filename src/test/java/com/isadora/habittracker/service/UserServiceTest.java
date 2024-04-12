ade5501c2d64e4af8b4a4ee72c29237c19a82dcc
Isadora Venkatasawmy <isadora.venkatasawmy@imgarena.com>
2024-04-12 18:18:26 +0100

    test: added UserServiceTest

---
+++ src/test/java/com/isadora/habittracker/service/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.isadora.habittracker.service;
+
+import com.isadora.habittracker.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private HabitService habitService;
+
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(userRepository, habitService);
+    }
+
+    @Test
+    void updateUserScore() {
+
+//        Mockito.doNothing().when(userRepository).saveUserScore(isA(Integer.class), isA(Integer.class));
+
+        userRepository.saveUserScore(33,1);
+
+        Mockito.verify(userRepository, Mockito.times(1)).saveUserScore(33, 1);
+
+    }
+
+    @Test
+    void calculateUserScore() {
+    }
+}
