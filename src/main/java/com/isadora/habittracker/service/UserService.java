9c5cfa66c00612dc1611f80438ca9e0a311753a1
Isadora Venkatasawmy <isadora.venkatasawmy@imgarena.com>
2024-04-15 13:47:17 +0100

    feature: added updateScore

--- src/main/java/com/isadora/habittracker/service/UserService.java
+++ src/main/java/com/isadora/habittracker/service/UserService.java
@@ -1,9 +1,10 @@
 package com.isadora.habittracker.service;
 
+import com.isadora.habittracker.domain.EntityNotFound;
+import com.isadora.habittracker.domain.Habit;
 import com.isadora.habittracker.domain.User;
 import com.isadora.habittracker.repository.UserRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,9 +12,11 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final HabitService habitService;
 
-    public UserService(final UserRepository userRepository) {
+    public UserService(final UserRepository userRepository, HabitService habitService) {
         this.userRepository = userRepository;
+        this.habitService = habitService;
     }
 
     public Iterable<User> listAllUsers() {
@@ -32,8 +35,58 @@ public class UserService {
         return userRepository.findAllInactiveUsers();
     }
 
-    @Transactional
+//    @Transactional
     public void updateUserScore(int score, int userId) {
         userRepository.saveUserScore(score, userId);
+    }
+
+    // when habit performed (pass HabitId),
+    // if counter=0 and Reward=L1, then it's a first time habit. -> Calculate score
+    // if counter=0 and Reward=L2, then it's not a first time habit. -> Calculate score
+    public int calculateUserScore(User loggedInUser, int habitId) {
+
+        int score = loggedInUser.getScore();
+//        var habit = habitService.listHabitById(habitId);
+//        if (habit.isPresent()) {
+//            rewardId = habit.get().getReward().getId();
+//
+//        }
+        if (habitService.listHabitById(habitId).isEmpty()) {
+            throw new EntityNotFound();
+        }
+
+        Habit habit = habitService.listHabitById(habitId).get();
+        int rewardId = habit.getReward().getId();
+        int difficultyPoints = habit.getDifficultyPoints();
+        int counter = habit.getCounter();
+
+        // if first time habit performed
+        if (counter == 0 && rewardId == 1) {
+            switch (difficultyPoints) {
+                case 1:
+                    score += 2;
+                    break;
+                case 2:
+                    score += 3;
+                    break;
+                case 3:
+                    score += 5;
+                    break;
+            }
+
+        } else if (counter == 0 && rewardId == 2) {
+            switch (difficultyPoints) {
+//                case 1:
+//                    score += 0;
+//                    break;
+                case 2:
+                    score += 3;
+                    break;
+                case 3:
+                    score += 5;
+                    break;
+            }
+        }
+        return score;
     }
 }
