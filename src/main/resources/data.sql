4bc20cb24ee8fea05a711cbd9677659d27157d67
IsadoraV13 <30869562+IsadoraV13@users.noreply.github.com>
2024-06-09 15:29:24 +0100

    refactor: removed specified id numbers when doing initial DB inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
-INSERT INTO reward (id, reward_name)
+INSERT INTO reward (reward_name)
 VALUES
-    (1, 'L1'), -- default
-    (2, 'L2'), -- in reality this would only be saved when a habit is accomplished the first time
-    (3, 'L3'), -- in reality this would only be saved when a habit is accomplished subsequent times but with no streak
-    (4, 'L4'); -- in reality this would only be saved when a habit is accomplished subsequent times but with a streak
+    ('L1'), -- default
+    ('L2'), -- when a habit is accomplished the first time
+    ('L3'), -- when a habit is accomplished subsequent times but with no streak
+    ('L4'); -- when a habit is accomplished subsequent times but with a streak
 
-INSERT INTO habit_user (id, username, score, is_active)
+INSERT INTO habit_user (username, score, is_active)
 VALUES
-    (1, 'user1', 0, 1),
-    (2, 'user2', 0, 1),
-    (3, 'user3', 0, 0);
+    ('user1', 0, 1),
+    ('user2', 0, 1),
+    ('user3', 0, 0);
 
-INSERT INTO habit (id, habit_name, user_id, reward_id, theme_id, difficulty_points, counter)
+INSERT INTO habit (habit_name, user_id, reward_id, theme_id, difficulty_points, counter)
 VALUES
-    (1, '3 x 3 push-ups', 1, 1, 1, 1, 0),
+    ('3 x 3 push-ups', 1, 1, 1, 1, 0),
 --    (2, '1 x 20 of something difficult', 1, 1, 1, 1, 0),
-    (3, 'code for 2h', 1, 1, 2, 1, 0);
+    ('code for 2h', 1, 1, 2, 1, 0);
 
-INSERT INTO theme (id, theme_name, user_id)
+INSERT INTO theme (theme_name, user_id)
 VALUES
-    (1, 'bada$$ fitness', 1),
-    (2, 'learning to code', 2);
+    ('bada$$ fitness', 1),
+    ('learning to code', 2);
